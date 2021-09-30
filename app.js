7f6c36b4b65aefff129dcf032065193c52120cab
Abu Hasan <hasanabubachelor@gmail.com>
2021-09-30 22:53:53 +0600

    First Class Section with addeventListener

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-
 document.getElementById("firstPlusBtn").addEventListener("click", function () {
-  const firstInput = document.getElementById("first-input").value;
-  let firstClassNumber = parseInt(firstInput);
-  //console.log(newFirstInput);
-  firstClassNumber++;
-  console.log(firstClassNumber);
+  const firstPlusInput = document.getElementById("firstInput");
+  const inputNumber = parseInt(firstPlusInput.value);
+  console.log(inputNumber);
+  const newPlusNumber = inputNumber + 1;
+  firstInput.value = newPlusNumber;
+  const numberIncrease = newPlusNumber * 150;
+  console.log(numberIncrease);
 });
+document.getElementById("firstMinusBtn").addEventListener("click", function () {
+  const firstMinusInput = document.getElementById("firstInput");
+  const inputNumber = parseInt(firstMinusInput.value);
+  const newMinusNumber = inputNumber - 1;
+  firstInput.value = newMinusNumber;
+  const numberDecrease = newMinusNumber * 150;
+  console.log(numberDecrease);
+
+});
+
